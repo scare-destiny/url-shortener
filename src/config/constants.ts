b98aae77444223a1a136f7749abae79d943fcf1f
scare-destiny <zhenya.venger@gmail.com>
2024-02-01 12:43:14 +0200

    x

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,7 +2,8 @@ export const PROD_BASE_URL = 'https://xlug.vercel.app'
 
 export const HOST_ICON_PLACEHOLDER = new URL('/assets/host-icon-placeholder.png', PROD_BASE_URL).toString()
 
-export const BASE_URL = process.env.NODE_ENV === 'production' ? PROD_BASE_URL : 'http://localhost:3000'
+export const BASE_URL =
+  process.env.NODE_ENV === 'production' ? PROD_BASE_URL : 'https://url-shortener-taupe-eta.vercel.app/'
 
 export const ICON_FROM_HOST_URL = 'https://unavatar.io/google'
 
